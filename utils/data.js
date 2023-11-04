742232257ebbc520b2eda473080ef77d71af8e25
Nehaps29 <133929172+Nehaps29@users.noreply.github.com>
2023-11-04 12:18:14 -0400

    added gitignore file

---
+++ utils/data.js
@@ -0,0 +1,26 @@
+const thoughtsData = [];
+  
+  const reactionData = [
+    {
+        reactionBody: "Lol",
+        username: "test1"
+    },
+    {
+        reactionBody: "Stop!",
+        username: "test2"
+    },
+    {
+        reactionBody: "Phew",
+        username: "test3"
+    },
+    {
+        reactionBody: "Yay!!",
+        username: "test4"
+    }
+  ];
+  
+  
+  
+  // Export the functions for use in seed.js
+  module.exports = { getRandomName, getRandomAssignments };
+  
