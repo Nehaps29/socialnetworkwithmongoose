3991b8e0b83d2b49be211aad9ce496a8ce1738e5
Nehaps29 <133929172+Nehaps29@users.noreply.github.com>
2023-11-04 21:24:14 -0400

    chnaged userController to chnage getUser and getSIngleUser so that it populates thoughts details as well

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,7 +5,7 @@ module.exports = {
   // Get all users
   async getUsers(req, res) {
     try {
-        const users = await User.find();
+        const users = await User.find().populate({ path: 'thoughts', select: '-__v' });
         res.json(users);
       }
        catch (err) {
@@ -16,8 +16,7 @@ module.exports = {
   // Get a single user
   async getSingleUser(req, res) {
     try {
-      const user = await User.findOne({ _id: req.params.userId })
-        .select('-__v');
+      const user = await User.findOne({ _id: req.params.userId }).populate({ path: 'thoughts', select: '-__v' });
 
       if (!user) {
         return res.status(404).json({ message: 'No user with that ID' })
